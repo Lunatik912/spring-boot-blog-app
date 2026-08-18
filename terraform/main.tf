72d64f7a43179361e262eb31690340e57434723d
Gagan Badiger <www.gaganbadiger373@gmail.com>
2026-08-18 21:26:59 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,3 +24,10 @@ resource "azurerm_subnet" "aks" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_container_registry" "main" {
+  name                = "springbootblogacr"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
