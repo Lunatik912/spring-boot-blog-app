876cc3cf5a90a8de9d889bbf0b1fe9216eaf9627
Gagan Badiger <www.gaganbadiger373@gmail.com>
2026-08-18 23:22:17 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,13 @@ variable "location" {
   description = "Azure region where resources will be created"
   type        = string
 }
+
+variable "container_app_name" {
+  description = "Name of the DEV Container App"
+  type        = string
+}
+
+variable "container_apps_environment_name" {
+  description = "Name of the Container Apps environment"
+  type        = string
+}
