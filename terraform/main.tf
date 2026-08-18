9d6ca4c5915dd2b9b5407b19af380bf96ad83d23
Gagan Badiger <www.gaganbadiger373@gmail.com>
2026-08-18 21:10:00 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,25 @@ resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_virtual_network" "main" {
+  name                = "spring-boot-blog-app-vnet"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "container_apps" {
+  name                 = "container-apps-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "aks" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
