b4c075143f087ac231e0f2de1f84c825ed2e968d
Gagan Badiger <www.gaganbadiger373@gmail.com>
2026-08-18 21:11:35 +0530

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "resource_group_name" {
+  description = "Name of the application resource group"
+  value       = azurerm_resource_group.main.name
+}
+
+output "vnet_name" {
+  description = "Name of the application VNet"
+  value       = azurerm_virtual_network.main.name
+}
+
+output "container_apps_subnet_id" {
+  description = "ID of the Container Apps subnet"
+  value       = azurerm_subnet.container_apps.id
+}
+
+output "aks_subnet_id" {
+  description = "ID of the AKS subnet"
+  value       = azurerm_subnet.aks.id
+}
